a43ffbdb29c89ded16841dda69755fc403b4c78a
Alex Nesov <magicofasword@gmail.com>
2025-03-09 12:01:52 +0200

    Singleton Mayer's

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
 
+class Singleton {
+	public:
+	
+	static Singleton& getInstance(){
+		static Singleton m_instance;
+		return m_instance;
+	}
+
+	private:
+	Singleton() = default;
+	~Singleton() = default;
+
+	Singleton(const Singleton& ) = delete;
+	Singleton(Singleton&& ) = delete;
+	Singleton& operator=(const Singleton& ) = delete;  
+};
+
+
 int main(){
-	std::cout<<"Hello from dp!!" <<std::endl;
+
+	std::cout << "Hello Singleton.\n";
+	Singleton& s = Singleton::getInstance();	
+
+
 	return 0;
 }
