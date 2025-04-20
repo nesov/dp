234bf66b213d7eb95b7305f46a4fee06ab0f7a19
Alex Nesov <magicofasword@gmail.com>
2025-04-20 15:51:40 +0300

    1. Two Sum

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,44 @@
+//1. Two Sum
+
 #include <iostream>
+#include <vector>
 
-   std::string mergeAlternately(std::string word1, std::string word2) {
+	std::vector<int> twoSum(std::vector<int>& nums, int target) {
+		std::vector<int> res;
+		size_t size = nums.size();
+		bool isDone {false};
 
-		int asize = word1.size();
-		int bsize = word2.size();
-
-		int maxsize = (asize > bsize)? asize: bsize;
-
-        std::string res;
-		res.reserve(asize+bsize);
-        for (int i = 0; i < maxsize; i++ ){
-			if (i < asize)
-            	res += word1[i];
-			if(i < bsize)
-				res += word2[i];
-        }
-        return res;
+		for (int i = 0; i < size; i++){
+			if (!isDone){
+				for (int j = i + 1; j < size; j++){	
+					if (nums[i] + nums[j] == target){
+						res.push_back(i);
+						res.push_back(j);
+						isDone = true;
+						break;
+					}	
+				}	
+			} else {
+				break;
+			}
+		}
+		return res;
     }
+
 
 int main()
 {
-	std::string a {"ace"};
-	std::string b {"bdfgpq"};
+	// std::vector<int> vec {2,7,11,15};
+	// std::vector<int> vec {3,2,4};
+	// std::vector<int> vec {3,3};
+	std::vector<int> vec {2,5,5,1};
 
-	std::cout <<mergeAlternately(a, b)<< std::endl;
+	std::vector<int> res =  twoSum(vec,10);
 
+
+	for (auto i : res){
+		std::cout << i << ", "; 
+	}
+	std::cout<<std::endl;
 	return 0;
 }
