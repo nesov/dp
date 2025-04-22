b476b9b55b0de0f34fa637d8a6b480f0b5b2903a
Alex Nesov <magicofasword@gmail.com>
2025-04-22 10:05:52 +0300

    26. Remove Duplicates from Sorted Array

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,19 @@
-//1. Two Sum
+//26. Remove Duplicates from Sorted Arra
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-	std::vector<int> twoSum(std::vector<int>& nums, int target) {
-		std::vector<int> res;
-		size_t size = nums.size();
-		bool isDone {false};
-
-		for (int i = 0; i < size; i++){
-			if (!isDone){
-				for (int j = i + 1; j < size; j++){	
-					if (nums[i] + nums[j] == target){
-						res.push_back(i);
-						res.push_back(j);
-						isDone = true;
-						break;
-					}	
-				}	
-			} else {
-				break;
-			}
-		}
-		return res;
-    }
+int removeDuplicates(std::vector<int>& nums) {
+	auto it = std::unique(nums.begin(), nums.end());
+    nums.erase(it, nums.end()); 
+	return nums.size();
+}
 
 
-int main()
-{
-	// std::vector<int> vec {2,7,11,15};
-	// std::vector<int> vec {3,2,4};
-	// std::vector<int> vec {3,3};
-	std::vector<int> vec {2,5,5,1};
-
-	std::vector<int> res =  twoSum(vec,10);
-
-
-	for (auto i : res){
-		std::cout << i << ", "; 
-	}
-	std::cout<<std::endl;
+int main(){
+	// std::vector<int> nums{1,1,2};
+	std::vector<int> nums{0,0,1,1,1,2,2,3,3,4};
+	removeDuplicates(nums);
 	return 0;
 }
