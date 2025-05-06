65b1f867f8429b10580a387710f0a23ab27ccd3a
Alex Nesov <magicofasword@gmail.com>
2025-05-06 10:43:12 +0300

    2. Add Two Numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,81 @@
-//27. Remove Element
+// 2. Add Two Numbers
 
 #include <iostream>
 #include <string>
 #include <cassert>
 
-int removeElement(std::vector<int>& nums, int val) {
-	nums.erase(std::remove(nums.begin(),nums.end(), val), nums.end());
-	return static_cast<int>(nums.size());  
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void print(ListNode* list) {
+	ListNode* temp = list;
+	while (temp){
+		std::cout << temp -> val << std::endl;
+		temp = temp -> next;
+	}
+}
+	
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode res (0);
+		ListNode *tail = &res; 
+		int temp {0};
+
+		while (l1 || l2 || temp){
+			int sum = temp;
+		
+			if(l1){
+				sum+=l1-> val; 
+				l1 = l1 -> next;
+			}
+			if (l2){
+				sum+=l2-> val; 
+				l2 = l2 -> next;
+			}
+	
+			temp = sum / 10;
+			tail -> next = new ListNode(sum % 10);
+			tail = tail->next;
+		}
+        return res.next;
 }
 
+
 int main(){
-	std::vector<int> a{3,2,2,3};
-	std::vector<int> b{0,1,2,2,3,0,4,2};
-	assert(2 == removeElement(a, 3));
-	assert(5 == removeElement(b, 2));
+	
+	ListNode* l1 =  new ListNode(2);
+	l1 ->next = new ListNode(4);
+	l1 ->next ->next = new ListNode(3);
+
+	// print(l1);
+
+	ListNode* l2 =  new ListNode(5);
+	l2 ->next = new ListNode(6);
+	l2 ->next ->next = new ListNode(4);
+	// print(l2);
+
+
+	ListNode* l3 = addTwoNumbers(l1, l2);
+	print(l3);
+	
+
+
 	return 0;
 }
