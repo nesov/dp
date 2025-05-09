0080a31fbc577590fc9713f12d67d8168183794c
Alex Nesov <magicofasword@gmail.com>
2025-05-09 18:55:31 +0300

    3343. Count Number of Balanced Permutations

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,98 @@
-//29. Divide Two Integers
+//3343. Count Number of Balanced Permutations
 
 #include <iostream>
 #include <cassert>
-#include <vector>
-#include <tuple>
+#include <string>
+#include <chrono>
 
+constexpr static long long MOD = 1e9 + 7;
 
-int divide(int dividend, int divisor) {
-    if (dividend == INT_MIN && divisor == -1) return INT_MAX;
-    if (divisor == 0) return -1;
-    if (divisor == 1) return dividend;
-    if (dividend == divisor) return 1;
-    if (dividend < divisor && dividend >= 0) return 0;
+class Timer {
+ public: 
+    Timer(){start = clock.now();}
+    ~Timer(){std::cout <<"Time: "<< duration_cast<std::chrono::milliseconds> (clock.now() - start) <<std::endl;}
+ private: 
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    std::chrono::high_resolution_clock clock;
+};
 
-    unsigned long long u_dividend = dividend < 0 ? -(long long)dividend : dividend;
-    unsigned long long u_divisor  = divisor  < 0 ? -(long long)divisor  : divisor;
-    unsigned long long result {0};
-    unsigned long long temp {0};
+int countBalancedPermutations(std::string num) {
+    int tot = 0, n = num.size();
+    std::vector<int> cnt(10);
+    for (char ch : num) {
+        int d = ch - '0';
+        cnt[d]++;
+        tot += d;
+    }
+    if (tot % 2 != 0) {
+        return 0;
+    }
 
-    for (int i = 31; i >= 0; --i) {
-        if (temp + (u_divisor << i) <= u_dividend) {
-            temp += u_divisor << i;
-            result |= 1ULL << i;
+    int target = tot / 2;
+    int maxOdd = (n + 1) / 2;
+    std::vector<std::vector<long long>> comb(maxOdd + 1, std::vector<long long>(maxOdd + 1));
+    std::vector<std::vector<long long>> f(target + 1, std::vector<long long>(maxOdd + 1));
+    for (int i = 0; i <= maxOdd; i++) {
+        comb[i][i] = comb[i][0] = 1;
+        for (int j = 1; j < i; j++) {
+            comb[i][j] = (comb[i - 1][j] + comb[i - 1][j - 1]) % MOD;
         }
     }
-    return (dividend < 0) ^ (divisor < 0) ? -static_cast<int>(result) : static_cast<int>(result);
+    f[0][0] = 1;
+    int psum = 0, totSum = 0;
+    for (int i = 0; i <= 9; i++) {
+        psum += cnt[i];
+        totSum += i * cnt[i];
+        for (int oddCnt = std::min(psum, maxOdd); oddCnt >= std::max(0, psum - (n - maxOdd)); oddCnt--) {
+            int evenCnt = psum - oddCnt;
+            for (int curr = std::min(totSum, target); curr >= std::max(0, totSum - target); curr--) {
+                long long res = 0;
+                for (int j = std::max(0, cnt[i] - evenCnt); j <= std::min(cnt[i], oddCnt) && i * j <= curr; j++) {
+                    long long ways = comb[oddCnt][j] * comb[evenCnt][cnt[i] - j] % MOD;
+                    res = (res + ways * f[curr - i * j][oddCnt - j] % MOD) % MOD;
+                }
+                f[curr][oddCnt] = res % MOD;
+            }
+        }
+    }
+    return f[target][maxOdd];
 }
 
-#define TESTS1
-
-int main(){
-
-    using DataSet =  std::vector<std::tuple<int,int,int,const char*> >;
-
-    DataSet data{
-        {10,3, 3, "Test1"},
-        {7,-3, -2,"Test2"},
-        {1, 2, 0, "Test3"},
-        {5, 2, 2,"Test4"},
-        {-2147483648, -1, 2147483647,"Test5"},
-        {-1, 1, -1,"Test6"},
-        {-2147483648, 2, -1073741824, "Test7"}
+#pragma region 
+void run() { 
+    std::vector<std::pair<std::string, int>> data {
+        {"123", 2},
+        {"112", 1},
+        {"12345",0},
+        {"523", 2},
+        {"9823935411945", 0}
     };
 
-    for (auto&i : data){
-        int res = divide(std::get<0>(i),std::get<1>(i));
-#ifdef TESTS
-        assert((std::get<2>(i) == res));
-#endif
-        std::cout<<std::get<3>(i)<<": "<< res <<std::endl;  
+    for (auto& item : data){
+        int res {0};
+        std::cout <<"Test data: "<<item.first <<std::endl; 
+        std::cout <<"Start running...\n";
+        {
+            Timer t;
+            res = countBalancedPermutations(item.first); 
+        }
+        std::cout <<"Finished.\n\n";
+        assert(item.second == res);
     }
+
+}
+#pragma endregion
+
+#define TESTS
+
+int main() {
+#ifdef TESTS
+    run();
+#else
+    {
+        Timer t;
+        std::cout <<  "countBalancedPermutations is : "<< countBalancedPermutations("0593136364172")<<std::endl;
+    }
+#endif
 	return 0;
 }
