411c79a8244421a16e3697ab17e799b28fa1a042
Alex Nesov <magicofasword@gmail.com>
2025-05-11 10:54:06 +0300

    509. Fibonacci Number

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,16 @@
-//66. Plus One
+//509. Fibonacci Number
 
 #include <iostream>
-#include <vector>
 
 
-std::vector<int> plusOne(std::vector<int>& digits) {
-    int n = digits.size();
-    for (int i = n - 1; i >= 0; --i) {
-        if (digits[i] < 9) {
-            digits[i] += 1;
-            return digits;
-        }
-        digits[i] = 0;
-    }
-    digits.insert(digits.begin(), 1);
-    return digits;
+int fib(int n) {
+    if (n == 1 ) return 1;
+    if (n == 0 ) return 0;  
+    return fib(n - 1) + fib(n - 2);
 }
 
 
 int main() {
-    std::vector<int> data {9,9};
-    data = plusOne(data);
-    for(auto& item : data) std::cout << item << ", ";
+    std::cout << fib(4);
 	return 0;
 }
