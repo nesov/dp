f7606eacfe8933c6551825e68a1455d62e90ab51
Alex Nesov <magicofasword@gmail.com>
2025-05-11 18:01:08 +0300

    1550. Three Consecutive Odds

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
-//509. Fibonacci Number
+//1550. Three Consecutive Odds
 
 #include <iostream>
 
+bool threeConsecutiveOdds2(std::vector<int>& arr) {
+    std::vector<int>::iterator iter = arr.begin();
+    while ((iter+2) != arr.end()){
+        if ((*iter % 2 == 1) && (*(iter+1) % 2 == 1) && (*(iter+2) % 2 == 1)){
+            return true;
+        }
+        iter++;
+    }
+    return false;
+}
 
-int fib(int n) {
-    if (n == 1 ) return 1;
-    if (n == 0 ) return 0;  
-    return fib(n - 1) + fib(n - 2);
+
+bool threeConsecutiveOdds1(std::vector<int>& arr){
+    for (size_t i = 0; i + 2 < arr.size(); ++i) {
+        if (arr[i] % 2 == 1 && arr[i + 1] % 2 == 1 && arr[i + 2] % 2 == 1) {
+            return true;
+        }
+    }
+    return false;
 }
 
 
 int main() {
-    std::cout << fib(4);
+    std::vector<int> arr {2,6,4,1};
+    std::vector<int> arr1 {1,2,34,3,4,5,7,23,12};
+
+    bool result = threeConsecutiveOdds1(arr);
+    std::cout << std::boolalpha << result << std::endl;
 	return 0;
 }
