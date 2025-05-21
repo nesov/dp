5c78d18b33bb6e15b296a82d87b20e58e6347627
Alex Nesov <magicofasword@gmail.com>
2025-05-21 08:57:15 +0300

    set and extract bytes from value

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
-#include <bitset>
 #include <iomanip>
+#include <assert.h>
 
 
-void print (uint32_t res) {
+
+inline void print (uint32_t res) {
     std::cout <<" : "<< std::bitset<32>(res) << std::endl;
 }
 
-int main(){
-
-    uint32_t a = 0xAABBCCDD;
-    uint32_t expected = 0xAAEECCDD;
+void taskOne(){
+    int32_t a = 0xAABBCCDD;
+    constexpr uint32_t expected = 0xAAEECCDD;
     uint8_t b = 0xEE;
-
     a = (a & 0xFF00FFFF) | (b << 16);
-    print(expected);
-    print(a);
+    assert(a == expected);
+} 
+
+inline uint8_t extractByte(uint32_t value, uint8_t index){
+    return (value >> (index * 8)) & 0xFF;
+}
+
+inline uint32_t setByte(uint32_t value, uint8_t byte, uint8_t index){
+    uint32_t mask = ~(0xFF << (index * 8));
+    return (value & mask) | (byte << (index * 8));
+}
+
+int main(){
+    constexpr uint32_t a = 0xAABBCCDD;
+    constexpr uint32_t expected = 0xAAEECCDD;
+    uint32_t result = setByte(a, 0xEE, 2);
+    assert(result  == expected);    
     return 0;
 }
